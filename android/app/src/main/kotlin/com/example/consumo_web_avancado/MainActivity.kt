fe4cd4485906e6420e1e78e402fb1656353dcd4c
peedrorof <peedrorof@gmail.com>
2022-11-25 11:00:23 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/consumo_web_avancado/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.consumo_web_avancado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:consumo_web_avancado/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
